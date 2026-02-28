db7fcb186f1c85b50c96f0b18b9b0383d62b2adc
CR7sewey <miguelmotacarvalho@gmail.com>
2026-02-28 18:49:47 +0000

    Personalised toast message with cb func to confirm the history delete

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -9,9 +9,12 @@ import useTaskContext from '../../context/useTaskContext'
 import { typeConversion, type TaskStateModel } from '../../models/TaskModel'
 import { formatSecondsToMinutes, getFormattedDate, getTaskStatus, sortTasksByField } from '../../utils/methods'
 import { TaskActionTypes } from '../../context/taskReducer'
+import { toast } from 'react-toastify'
+import { showMessage } from '../../adapter/showMessage'
 
 export const History = () => {
   const { task, dispatch, setBtn } = useTaskContext();
+  const [historyCleared, setHistoryCleared] = useState(false); // state to track if history has been cleared
   const [historyTasks, setHistoryTasks] = useState(() => {
     return {
       tasks: [...task.tasks],
@@ -22,7 +25,7 @@ export const History = () => {
 
   useEffect(() => {
     setHistoryTasks(prev => ({ ...prev, tasks: [...task.tasks] }));
-  }, [task.tasks]);
+  }, [task.tasks, historyCleared]); // re-run effect when task.tasks or historyCleared changes to update the displayed tasks after clearing history
   // Omit<typeof historyTasks, 'tasks' | 'direction'> - We only need the tasks array and sorting info for this component, so we can omit the other properties from the state model
   const orderByParameter = ({ field }: Pick<typeof historyTasks, 'field'>) => {
     setHistoryTasks(prev => ({ ...prev, field, direction: prev.direction === 'asc' ? 'desc' : 'asc' }));
@@ -41,8 +44,34 @@ export const History = () => {
   };
 
   function clearHistory() {
-    dispatch({ type: TaskActionTypes.RESET_STATE });
-    setBtn(false);
+    showMessage.dismiss(); // dismiss any existing toasts before showing a new one
+    // disable btn
+    const btn = document.getElementById('clear-history');
+    if (btn) {
+      btn.setAttribute('disabled', 'true');
+      btn.blur(); // remove focus from the button after clicking to prevent accidental multiple clicks
+    }
+    /*toast("Hello world!", {
+      onClose: (reason) => {
+        console.log('Toast closed - ' + reason);
+      }
+    });*/
+    showMessage.confirm('History cleared', (confirmation: boolean) => {
+      if (!confirmation) {
+        if (btn) {
+          btn.removeAttribute('disabled');
+        }
+        return;
+      }
+      dispatch({ type: TaskActionTypes.RESET_STATE });
+      setHistoryCleared(prev => !prev); // toggle state to trigger re-render and update displayed tasks
+      setBtn(false);
+    });
+    /*if (window.confirm('Are you sure you want to clear the history? This action cannot be undone.')) {
+      dispatch({ type: TaskActionTypes.RESET_STATE });
+      setBtn(false);
+    }*/
+
   }
 
   return (
@@ -53,6 +82,7 @@ export const History = () => {
           <span className={styles.buttonContainer}>
             {task.tasks.length > 0 && (
               <DefaultButton
+                id='clear-history'
                 icon={<TrashIcon />}
                 color='red'
                 aria-label='Apagar todo o histórico'
@@ -64,34 +94,35 @@ export const History = () => {
         </Heading>
       </Container>
       <Container>
-        <div className={styles.responsiveTable}>
-          <table>
-            <thead>
-              <tr>
-                <th onClick={() => orderByParameter({ field: 'task' })} className={styles.thSorted + (historyTasks.field === 'task' ? ` ${styles.selected}` : '')}>Task ↕</th>
-                <th onClick={() => orderByParameter({ field: 'duration' })} className={styles.thSorted + (historyTasks.field === 'duration' ? ` ${styles.selected}` : '')}>Duration ↕</th>
-                <th onClick={() => orderByParameter({ field: 'date' })} className={styles.thSorted + (historyTasks.field === 'date' ? ` ${styles.selected}` : '')}>Date ↕</th>
-                <th onClick={() => orderByParameter({ field: 'status' })} className={styles.thSorted + (historyTasks.field === 'status' ? ` ${styles.selected}` : '')}>Status ↕</th>
-                <th onClick={() => orderByParameter({ field: 'type' })} className={styles.thSorted + (historyTasks.field === 'type' ? ` ${styles.selected}` : '')}>Type ↕</th>
-              </tr>
-            </thead>
-            <tbody>
-              {historyTasks.tasks.map((t, index) => (
-                <tr key={t.id}>
-                  <td>{t.task}</td>
-                  <td>{t.duration}</td>
-                  <td>{getFormattedDate(t.startDate)}</td>
-                  <td>
-                    {getTaskStatus(t, task.activeTask)}
-                  </td>
-                  <td>{typeConversion[t.type]}</td>
+        {historyTasks.tasks.length !== 0 ?
+          <div className={styles.responsiveTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th onClick={() => orderByParameter({ field: 'task' })} className={styles.thSorted + (historyTasks.field === 'task' ? ` ${styles.selected}` : '')}>Task ↕</th>
+                  <th onClick={() => orderByParameter({ field: 'duration' })} className={styles.thSorted + (historyTasks.field === 'duration' ? ` ${styles.selected}` : '')}>Duration ↕</th>
+                  <th onClick={() => orderByParameter({ field: 'date' })} className={styles.thSorted + (historyTasks.field === 'date' ? ` ${styles.selected}` : '')}>Date ↕</th>
+                  <th onClick={() => orderByParameter({ field: 'status' })} className={styles.thSorted + (historyTasks.field === 'status' ? ` ${styles.selected}` : '')}>Status ↕</th>
+                  <th onClick={() => orderByParameter({ field: 'type' })} className={styles.thSorted + (historyTasks.field === 'type' ? ` ${styles.selected}` : '')}>Type ↕</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-
-
+              </thead>
+              <tbody>
+                {historyTasks.tasks.map((t, index) => (
+                  <tr key={t.id}>
+                    <td>{t.task}</td>
+                    <td>{t.duration}</td>
+                    <td>{getFormattedDate(t.startDate)}</td>
+                    <td>
+                      {getTaskStatus(t, task.activeTask)}
+                    </td>
+                    <td>{typeConversion[t.type]}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          : <p style={{ textAlign: 'center', fontWeight: 'bold' }}>No history yet. Start a task to see it here!</p>
+        }
       </Container>
     </MainTemplate>
   )
